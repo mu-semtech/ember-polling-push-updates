5cef34c8f1a581957efacc44f363a2e28e4dc3ab
Aad Versteden <madnificent@gmail.com>
2025-10-17 19:08:59 +0200

    Basic route for monitoring an individual resource
    When the resource is fetched by a model hook, this approach will quickly provide
    us with an easy setup.

---
+++ addon/routes/resource-uri-monitoring.js
@@ -0,0 +1,21 @@
+import { service } from '@ember/service';
+import Route from '@ember/routing/route';
+
+export default class ResourceUriMonitoring extends Route {
+  @service pushUpdates;
+
+  // Model for deregistering monitoring on unload
+  lastModel = null;
+
+  async setupController(_controller, model) {
+    super.setupController(...arguments);
+    this.lastModel && await this.pushUpdates.unMonitorModel(this.lastModel);
+    this.lastModel = model;
+    await this.pushUpdates.monitorModel( model );
+  }
+
+  async deactivate() {
+    super.deactivate(...arguments);
+    await this.pushUpdates.unMonitorModel( this.lastModel );
+  }
+}
---
+++ app/routes/resource-uri-monitoring.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-polling-push-updates/routes/resource-uri-monitoring';
